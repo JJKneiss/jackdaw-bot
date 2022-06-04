ae9a573ad1a20f1f1a01e9a6b97f5fe5d2101459
JJKneiss <54962857+JJKneiss@users.noreply.github.com>
2022-06-03 23:47:10 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# jackdaw-bot
+# Welcome to Jackdaw!
+
+## A smart, multipurpose discord bot soon to be integrated with the Project Mouseion library. 
+
+### Upcoming features include but are not limited to:
+
+* Moderation Commands
+* fetching Quote of the Day
+* fetching Project Mouseion resources
+* 
+
---
+++ bot.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const resources = require('./resources.json');
+const Discord = require('discord.js');
+const ms = require('ms');
+const { Client, Intents } = Discord;
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}.`);
+});
+
+client.on('messageCreate', msg => {
+    console.log(msg);
+    if (msg.content === "ping") {
+        msg.reply('pong!');
+    }
+    else if (msg.content === "!resources") {
+    }
+
+});
+
+client.login(process.env.BOT_TOKEN);
